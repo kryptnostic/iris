326d6142d8a68f8c2944f7f5704dcec823f4cb67
Nick Hewitt <nick@kryptnostic.com>
2014-10-22 17:59:50 -0700

    changed SCM store to a LocalKeyStore that currently handles storing and retrieving PrivateKey from a DataStore. Added unit tests for key storage and retrieval

--- src/main/java/com/kryptnostic/api/v1/client/LocalKeyStore.java
+++ src/main/java/com/kryptnostic/api/v1/client/LocalKeyStore.java
@@ -3,35 +3,36 @@ package com.kryptnostic.api.v1.client;
 import java.io.IOException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kryptnostic.crypto.PrivateKey;
 import com.kryptnostic.crypto.PublicKey;
+import com.kryptnostic.crypto.v1.ciphers.CryptoService;
+import com.kryptnostic.kodex.v1.models.AesEncryptable;
 import com.kryptnostic.kodex.v1.models.FheEncryptable;
 import com.kryptnostic.kodex.v1.security.SecurityConfigurationMapping;
 import com.kryptnostic.kodex.v1.storage.DataStore;
 
-public class SecurityConfigurationStore {
+/**
+ * Class to support storing private keys on device.
+ * @author Nick Hewitt &lt;nick@kryptnostic.com&gt;
+ *
+ */
+public class LocalKeyStore {
     private final DataStore store;
     private final ObjectMapper mapper;
 
-    public SecurityConfigurationStore(ObjectMapper mapper) {
+    public LocalKeyStore(ObjectMapper mapper) {
         this.store = new FileStore("keys");
         this.mapper = mapper;
     }
-
-    // TODO serialize each key into a byte array and store it
-    public void storeKeys(SecurityConfigurationMapping mapping) throws IOException {
-        // extract each key class and store it
-        PublicKey pubKey = mapping.get(FheEncryptable.class, PublicKey.class);
-        storeValue(pubKey.getClass().getCanonicalName(), pubKey);
+    
+    public void storeFhePrivateKey(PrivateKey fheKey) throws IOException {
+        storeValue(PrivateKey.class.getCanonicalName(), fheKey);
     }
     
-    public SecurityConfigurationMapping getMapping() throws IOException {
-        PublicKey pubKey = retrieveValue(PublicKey.class.getCanonicalName(), PublicKey.class);
-        return null;
+    public PrivateKey retrieveFhePrivateKey() throws IOException {
+        return retrieveValue(PrivateKey.class.getCanonicalName(), PrivateKey.class);    
     }
 
-    /**
-     * Serialize object to byte array with Jackson and store it in DataStore.
-     */
     private void storeValue(String keyString, Object value) throws IOException {
         byte[] data = mapper.writeValueAsBytes(value);
         byte[] key = keyString.getBytes();
