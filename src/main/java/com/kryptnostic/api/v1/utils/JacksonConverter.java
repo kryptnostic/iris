a11adfd31504c018d96d89a7ccc5a60613790998
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-09-27 19:22:48 -0700

    Added support for octet stream in converter

--- src/main/java/com/kryptnostic/api/v1/utils/JacksonConverter.java
+++ src/main/java/com/kryptnostic/api/v1/utils/JacksonConverter.java
@@ -2,10 +2,10 @@ package com.kryptnostic.api.v1.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,19 +22,22 @@ import com.kryptnostic.kodex.v1.crypto.keys.CryptoServiceLoader;
 import com.kryptnostic.kodex.v1.serialization.jackson.KodexObjectMapperFactory;
 
 /**
- * A {@link Converter} which uses Jackson for reading and writing entities.
+ * A {@link Converter} that handles both json and byte arrays. Based off work by Kai Waldron
+ * (kaiwaldron@gmail.com)
+ * 
+ * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
  *
- * @author Kai Waldron (kaiwaldron@gmail.com)
  */
 public class JacksonConverter implements Converter {
-    private static final String MIME_TYPE = "application/json; charset=UTF-8";
-    private static final Logger logger = LoggerFactory.getLogger( JacksonConverter.class );
+    private static final String JSON_MIME_TYPE = "application/json; charset=UTF-8";
+    private static final String BYTE_MIME_TYPE = "application/octet-stream";
+    private static final Logger logger         = LoggerFactory.getLogger( JacksonConverter.class );
     private final ObjectMapper  objectMapper;
 
     public JacksonConverter() {
         this.objectMapper = KodexObjectMapperFactory.getObjectMapper();
     }
-    
+
     public JacksonConverter( CryptoServiceLoader securityConfig ) {
         this.objectMapper = KodexObjectMapperFactory.getObjectMapper( securityConfig );
     }
@@ -42,32 +45,40 @@ public class JacksonConverter implements Converter {
     @Override
     public Object fromBody( TypedInput body, Type type ) throws ConversionException {
         try {
+            if ( StringUtils.equals( body.mimeType(), BYTE_MIME_TYPE ) ) {
+                return IOUtils.toByteArray( body.in() );
+            }
+
             JavaType javaType = objectMapper.getTypeFactory().constructType( type );
+
             InputStream in = body.in();
+
             if ( in.available() == 0 ) {
                 return null;
             }
+
             return objectMapper.readValue( body.in(), javaType );
         } catch ( IOException e ) {
-            logger.error( "Unable to deserialize object of type {}", type );
-            try {
-                logger.error("Representation: {}" , IOUtils.toString( body.in() ) );
-            } catch ( IOException e1 ) {
-                throw new ConversionException( e1 );
-            }
+            logger.error( "Unable to deserialize object of type {} from body with mime-type {}.",
+                    type,
+                    body.mimeType(),
+                    e );
             throw new ConversionException( e );
         }
     }
 
     @Override
     public TypedOutput toBody( Object object ) {
+        if ( byte[].class.isAssignableFrom( object.getClass() ) ) {
+            return new TypedByteArray( BYTE_MIME_TYPE, (byte[]) object );
+        }
         try {
-            String json = objectMapper.writeValueAsString( object );
-            return new TypedByteArray( MIME_TYPE, json.getBytes( "UTF-8" ) );
+            byte[] json = objectMapper.writeValueAsBytes( object );
+            return new TypedByteArray( JSON_MIME_TYPE, json );// json.getBytes( "UTF-8" )
         } catch ( JsonProcessingException e ) {
             throw new AssertionError( e );
-        } catch ( UnsupportedEncodingException e ) {
-            throw new AssertionError( e );
-        }
+        } /*
+           * catch ( UnsupportedEncodingException e ) { throw new AssertionError( e ); }
+           */
     }
 }
