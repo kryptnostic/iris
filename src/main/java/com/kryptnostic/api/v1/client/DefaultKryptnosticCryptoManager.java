31ee21d9fbdd90974fd28077ea62b8d417e711f0
Drew <drew@kryptnostic.com>
2015-11-09 19:44:10 -0800

    use update rsa public key call

--- src/main/java/com/kryptnostic/api/v1/client/DefaultKryptnosticCryptoManager.java
+++ src/main/java/com/kryptnostic/api/v1/client/DefaultKryptnosticCryptoManager.java
@@ -107,7 +107,7 @@ public class DefaultKryptnosticCryptoManager implements KryptnosticCryptoManager
             public RsaCompressingEncryptionService apply( UUID input ) {
                 try {
                     return new RsaCompressingEncryptionService( RsaKeyLoader.CIPHER, new PublicKeyEnvelope(
-                            keyStorageApi.getPublicKey(
+                            keyStorageApi.getRSAPublicKey(
                                     input ) ).asRsaPublicKey() );
                 } catch (
                         InvalidKeySpecException
