a1bd949d1d8d0bf8b788ceb674f2077d3478e413
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-11-09 21:14:41 -0800

    Upgraded search api.

--- src/main/java/com/kryptnostic/api/v1/search/DefaultSearchClient.java
+++ src/main/java/com/kryptnostic/api/v1/search/DefaultSearchClient.java
@@ -8,14 +8,13 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.kryptnostic.api.v1.KryptnosticConnection;
-import com.kryptnostic.api.v1.KryptnosticCryptoManager;
 import com.kryptnostic.api.v1.indexing.SimpleIndexer;
 import com.kryptnostic.kodex.v1.indexing.Indexer;
 import com.kryptnostic.kodex.v1.indexing.analysis.Analyzer;
 import com.kryptnostic.search.v1.SearchClient;
-import com.kryptnostic.search.v1.http.SearchApi;
 import com.kryptnostic.search.v1.models.request.SearchRequest;
-import com.kryptnostic.search.v1.models.response.SearchResultResponse;
+import com.kryptnostic.v2.search.SearchApi;
+import com.kryptnostic.v2.search.SearchResultResponse;
 
 /**
  * Default implementation of SearchService. Must use same IndexingService as the KryptnosticConnection.
